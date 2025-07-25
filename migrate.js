b01b71ba0951b023e4dac1c2ee590b804c9b0696
Eugene Shilin <eugeny.shilin@gmail.com>
2025-07-25 11:22:35 +0200

    fix: remove logging in migrations

--- migrate.js
+++ migrate.js
@@ -6,7 +6,6 @@ async function migrate(sequelize) {
       migrations: {glob: 'src/migrations/*.js'},
       context: sequelize.getQueryInterface(),
       storage: new SequelizeStorage({ sequelize }),
-      logger: console,
   });
 
   await umzug.up();
