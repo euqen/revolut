0bde84774202814342069525295cf157463a2350
Eugene Shilin <eugeny.shilin@gmail.com>
2025-07-25 11:28:16 +0200

    fix: update tests to work on CI, round 5

--- tests/setup.js
+++ tests/setup.js
@@ -2,11 +2,11 @@ import { jest } from '@jest/globals';
 
 global.console = {
   ...console,
-  log: jest.fn(),
-  debug: jest.fn(),
-  info: jest.fn(),
-  warn: jest.fn(),
-  error: jest.fn(),
+  // log: jest.fn(),
+  // debug: jest.fn(),
+  // info: jest.fn(),
+  // warn: jest.fn(),
+  // error: jest.fn(),
 };
 
 jest.setTimeout(10000);
