72bdbe7a6822292d6dc9abfe7fc848edd8c25ff3
Eugene Shilin <eugeny.shilin@gmail.com>
2025-07-26 12:48:59 +0200

    fix: fix connection param and local setup

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,11 @@
-import * as dotenv from 'dotenv'
-
-dotenv.config() 
-
 export default {
     port: process.env.PORT || 3003,
     db: {
-        dialect: process.env.DB_DIALECT || 'sqlite',
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || 'revolut-hello-app-db',
+        dialect: process.env.DB_DIALECT || 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     }
 };
